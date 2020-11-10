d9c2757d13db034432fce2f7dc52da3957e3bcfa
Tabang97 <tabangduda97@gmail.com>
2020-11-10 16:37:57 +0200

    finish later

---
+++ intro.js
@@ -0,0 +1,8 @@
+function Total_cost(x, y){
+    x=parseInt(document.getElementById('noficreamuys').value);
+    y=parseFloat(document.getElementById('cost').value)
+    // var answer=document.getElementById('Result')
+    answer = x*y;
+    alert("The total cost is:" + answer)
+}
+
